c871da5a847c82716cbf1dd30442d633a539c8a5
Xian Zheng <seeinplay@gmail.com>
2023-11-18 13:33:30 +0300

    chore: basic safe multisig integration

---
+++ app/lib/utils/SafeApiKit.ts
@@ -0,0 +1,31 @@
+import { ethers } from 'ethers'
+import {
+  EthersAdapter,
+  SafeAccountConfig,
+  SafeFactory,
+} from '@safe-global/protocol-kit'
+
+const provider = new ethers.providers.Web3Provider((window as any).ethereum)
+const safeOwner = provider.getSigner(0)
+
+const ethAdapter = new EthersAdapter({
+  ethers,
+  signerOrProvider: safeOwner,
+})
+
+const txServiceUrl = 'https://safe-transaction-mainnet.safe.global'
+
+const safeFactory = await SafeFactory.create({ ethAdapter })
+// safeAccountConfig, saltNonce, options, callback
+
+const owner1Signer = new ethers.Wallet('Wallet', provider)
+
+const safeAccountConfig: SafeAccountConfig = {
+  owners: [await owner1Signer.getAddress()],
+  threshold: 2,
+  // ... (Optional params)
+}
+
+const safe = await safeFactory.deploySafe({
+  safeAccountConfig,
+})
